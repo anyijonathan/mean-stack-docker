0792fa5c9b420975f03e1d14121c2cc795952528
anyijonathan <38350589+anyijonathan@users.noreply.github.com>
2023-07-23 16:35:27 +0100

    Update api.js

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -1,4 +1,3 @@
-// Import dependencies
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -20,41 +19,43 @@ const User = mongoose.model('User', userSchema);
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-		res.send('api works');
+  res.send('api works');
 });
 
 /* GET all users. */
-router.get('/users', (req, res) => {
-	User.find({}, (err, users) => {
-		if (err) res.status(500).send(error)
-
-		res.status(200).json(users);
-	});
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-/* GET one users. */
-router.get('/users/:id', (req, res) => {
-	User.findById(req.params.id, (err, users) => {
-		if (err) res.status(500).send(error)
-
-		res.status(200).json(users);
-	});
+/* GET one user. */
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 /* Create a user. */
-router.post('/users', (req, res) => {
-	let user = new User({
-		name: req.body.name,
-		age: req.body.age
-	});
-
-	user.save(error => {
-		if (error) res.status(500).send(error);
-
-		res.status(201).json({
-			message: 'User created successfully'
-		});
-	});
+router.post('/users', async (req, res) => {
+  try {
+    const { name, age } = req.body;
+    const user = new User({ name, age });
+    await user.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
